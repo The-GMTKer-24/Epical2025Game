a45460188b796ad6456666b5832d21fc445c62d8
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2025-07-31 12:17:38 -0700

    Basic things

--- Assets/Scripts/Factory Elements/Blocks/ItemSource.cs
+++ Assets/Scripts/Factory Elements/Blocks/ItemSource.cs
@@ -1,44 +1,48 @@
-using Factory_Elements;
 using Factory_Elements.Settings;
 using Scriptable_Objects;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class ItemSource : MonoBehaviour, IFactoryElement
+namespace Factory_Elements.Blocks
 {
-    [SerializeField] private FactoryElementType type;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class ItemSource : MonoBehaviour, IFactoryElement
     {
+        [SerializeField] private FactoryElementType type;
+        [SerializeField] private ResourceSet pool;
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
         
-    }
+        }
 
-    public int2 Position { get; set; }
-    public FactoryElementType FactoryElementType { get => type; }
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public int2 Position { get; set; }
+        public FactoryElementType FactoryElementType { get => type; }
     
-    public void OnNeighborUpdate(IFactoryElement newNeighbor, bool added)
-    {
-        Debug.Log($"{newNeighbor.FactoryElementType.name} has been changed");
-    }
+        public void OnNeighborUpdate(IFactoryElement newNeighbor, bool added)
+        {
+            Debug.Log($"{newNeighbor.FactoryElementType.name} has been changed");
+        }
 
-    public bool AcceptsResource(IFactoryElement sender, Resource resource)
-    {
-        throw new System.NotImplementedException();
-    }
+        public bool AcceptsResource(IFactoryElement sender, Resource resource)
+        {
+            return false;
+        }
 
-    public bool TryInsertResource(IFactoryElement sender, Resource resource)
-    {
-        throw new System.NotImplementedException();
-    }
+        public bool TryInsertResource(IFactoryElement sender, Resource resource)
+        {
+            return false;
+        }
 
-    public ISetting[] GetSettings()
-    {
-        throw new System.NotImplementedException();
+        public ISetting[] GetSettings()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
