6256f1b6432e26b2902859863b732a6be041d58b
nilederg <nilederg@gmail.com>
2025-08-01 00:01:56 -0700

    Create conveyor belts

---
+++ Assets/Scripts/Factory Elements/Blocks/ConveyorBelt.cs
@@ -0,0 +1,144 @@
+using System.Collections.Generic;
+using System.Linq;
+using Factory_Elements;
+using Factory_Elements.Settings;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Factory_Elements.Blocks
+{
+    public class ConveyorBelt : Block
+    {
+        public const int CAPACITY = 4;
+        public const float SPEED = 1.5f;
+        private const float MinDistance = 1.0f / CAPACITY;
+        private LinkedList<BeltItem> items; // First is most recently added (last to leave the belt), Last is least recently added (first to leave the belt)
+        private ConveyorBelt aheadBelt;
+        protected ElementSettings<Direction> directionSetting; 
+
+        void Awake()
+        {
+            items = new LinkedList<BeltItem>();
+            aheadBelt = null;
+            directionSetting = new ElementSettings<Direction>(Direction.North, "Direction", "The direction of the conveyor.");
+        }
+
+        public override void OnNeighborUpdate(IFactoryElement newNeighbor, bool added)
+        {
+            base.OnNeighborUpdate(newNeighbor, added);
+            
+            // Update aheadBelt
+            if (!added && newNeighbor is ConveyorBelt && newNeighbor == aheadBelt)
+            {
+                aheadBelt = null;
+            }
+
+            if (added && newNeighbor is ConveyorBelt newBelt)
+            {
+                int2 relativePos = newBelt.Position - this.Position;
+                int2 north = new int2(0, 1);
+                int2 east = new int2(1, 0);
+                int2 south = new int2(0, -1);
+                int2 west = new int2(-1, 0);
+                if (relativePos.Equals(north) && directionSetting.Value == Direction.North
+                    || relativePos.Equals(east) && directionSetting.Value == Direction.East
+                    || relativePos.Equals(south) && directionSetting.Value == Direction.South
+                    || relativePos.Equals(west) && directionSetting.Value == Direction.West)
+                {
+                    aheadBelt = newBelt;
+                }
+            }
+        }
+
+        public override bool AcceptsResource(IFactoryElement sender, Resource resource)
+        {
+            float room = items.First.Value.Progress;
+            return room >= MinDistance;
+        }
+
+        public override bool TryInsertResource(IFactoryElement sender, Resource resource)
+        {
+            if (!AcceptsResource(sender, resource)) return false;
+            if (resource is not Item) return false;
+            items.AddFirst(new BeltItem((Item) resource, 0.0f));
+            // TODO: Create item game object (possibly modify BeltItem constructor)
+            return true;
+        }
+
+        void FixedUpdate()
+        {
+            float aheadProgress = 1.0f;
+            if (aheadBelt && aheadBelt.items.Count != 0)
+            {
+                aheadProgress = aheadBelt.items.First.Value.Progress;
+            }
+            
+            float deltaDistance = Time.fixedDeltaTime * SPEED;
+
+            List<BeltItem> itemList = items.ToList();
+            List<BeltItem> markedForRemoval = new List<BeltItem>();
+            for (int i = itemList.Count - 1; i >= 0; i--)
+            {
+                float distance;
+                if (i == itemList.Count - 1)
+                {
+                    distance = 1 - itemList[i].Progress + aheadProgress;
+                    if (distance > MinDistance)
+                    {
+                        itemList[i].Progress += deltaDistance;
+                    }// Otherwise gets stuck
+                }
+                else
+                {
+                    float farthestAllowableProgress = itemList[i + 1].Progress - MinDistance;
+                    float farthestReachableProgress = itemList[i].Progress + deltaDistance;
+                    float newProgress = Mathf.Min(farthestAllowableProgress, farthestReachableProgress);
+                    itemList[i].Progress = newProgress;
+                }
+
+                if (itemList[i].Progress > 1.0f)
+                {
+                    if (aheadBelt.TryInsertResource(this, itemList[i].Item))
+                    {
+                        markedForRemoval.Add(itemList[i]);
+                    }
+                    else
+                    {
+                        itemList[i].Progress = 1.0f;
+                    }
+                }
+            }
+            
+            items = new LinkedList<BeltItem>(itemList);
+
+            foreach (BeltItem beltItem in markedForRemoval)
+            {
+                items.Remove(beltItem);
+                // TODO: Kill item game object
+            }
+        }
+
+        void Update()
+        {
+            // TODO: Move all belt items on this conveyor (child objects) to where their progress would indicate
+        }
+
+        public override ISetting[] GetSettings()
+        {
+            return new ISetting[] { directionSetting };
+        }
+    }
+}
+
+class BeltItem
+{
+    public Item Item;
+    public float Progress; // 0-1
+    // TODO: Paired with a child object of the conveyor belt
+
+    public BeltItem(Item item, float progress)
+    {
+        Item = item;
+        Progress = progress;
+    }
+}
