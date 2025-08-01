6ad3a6f18e738c9bdb0731c8825ed0d91e23e135
Plaonder <39774970+Plaonder@users.noreply.github.com>
2025-08-01 15:43:30 -0700

    Basic Main Menu Implementation

---
+++ Assets/Scripts/UI/Main Menu/MenuButtons.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenu
+{
+    public class MenuButtons : MonoBehaviour
+    {
+        public string sceneToLoad = "Main";
+        
+        public void PlayGame()
+        {
+            Debug.Log("The system will play game now!");
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        
+        public void QuitGame()
+        {
+            Debug.Log("The system will shut down now!");
+            #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
