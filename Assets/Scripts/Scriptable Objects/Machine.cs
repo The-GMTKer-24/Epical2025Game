9ce832772d9cd925b0527fe3bfab39823c5140f5
nilederg <nilederg@gmail.com>
2025-08-03 00:46:03 -0700

    Simplify machine

--- Assets/Scripts/Scriptable Objects/Machine.cs
+++ Assets/Scripts/Scriptable Objects/Machine.cs
@@ -7,14 +7,8 @@ namespace Scriptable_Objects
     [CreateAssetMenu(fileName = "New Machine", menuName = "Factory/Machine", order = 0)]
     public class Machine : FactoryElementType
     {
-        [SerializeField] private List<ResourceQuantity> itemCost;
         [SerializeField] private Recipe[] recipes;
-        [SerializeField] private int capacity;
 
         public Recipe[] Recipes => recipes;
-
-        public int Capacity => capacity;
-        
-        public List<ResourceQuantity> ItemCost => itemCost;
     }
 }
